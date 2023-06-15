53403314df00cfdadae35bb9f8d99579d155d1e5
Kirah Sapong <kirah@tbd.email>
2023-06-15 11:17:00 -0700

    Update credential manifest detail page styling

--- src/pages/Admin/Credentials/views/CredentialManifests/Details/Details.tsx
+++ src/pages/Admin/Credentials/views/CredentialManifests/Details/Details.tsx
@@ -37,63 +37,66 @@ const Details: Component<{ credential }> = (props) => {
     return (
         <div class="details-page">
             <div class="details-container">
-            <section class="render-panel">
-                    <div class="details-page-hero-card"
-                        style={{
-                            background: credential_manifest.output_descriptors.styles?.background.color || credential_manifest.issuer.styles?.background.color,
-                            color: credential_manifest.output_descriptors.styles?.text.color || credential_manifest.issuer.styles?.text.color
-                        }}>
-                        <div class="details-page-hero-card-content">
-                            <div class="details-page-hero-card-content-hero details-page-hero-card-full-width">
-                                <img src={credential_manifest.output_descriptors.styles?.hero.url || TBDWordmark} alt={credential_manifest.output_descriptors.styles?.hero.alt || "Generic abstract credential hero image"} />
-                            </div>
-                            <div class="details-page-hero-card-content-thumbnail">
-                                <img src={credential_manifest.output_descriptors.styles?.thumbnail.url || TBDLogoSquare} alt={credential_manifest.output_descriptors.styles?.thumbnail.alt || "Generic abstract credential thumbnail image"} />
-                            </div>
-                            <div class="details-page-hero-card-content-info">
-                                {schemaData && Object.keys(schemaData).map(label => {
-                                    return (
-                                        <div class="info-entry">
-                                            <div class="info-entry-label">{label}</div>
-                                            <div class="info-entry-data">Placeholder</div>
-                                        </div>
-                                    )
-                                })}
-                                <div class="info-entry">
-                                    <div class="info-entry-label">Issuer</div>
-                                    <div class="info-entry-data">{credential_manifest.issuer.name}</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="details-page-hero-card-content">
-                            <div class="details-page-hero-card-content-header">{credential_manifest.name}</div>
-                            <div class="details-page-hero-card-content-body">Verifiable Credential</div>
-                        </div>
-                    </div>
-                </section>
                 
                 <section class="details-page-hero panel">
                     <div class="details-page-hero-content">
-                        <p class="eyebrow-label">Credential Template</p>
-                        <h1>{credential_manifest.name}</h1>
-                        <p class="details-description">{credential_manifest.description}</p>
-                        <p>
-                            <a class="item-panel-card-link" href={`${location.origin}/apply/${credential_manifest.id}`} target="_blank">
-                                Application URL <Icon svg={ExternalArrow} />
-                            </a>
-                        </p>
-                        <div class="details-page-hero-content-actions button-row">
-                            <IssueModal content={{
-                                button: {
-                                    className: "secondary-button",
-                                    label: "Issue"
-                                },
-                                manifestId: params.id,
-                                schemaId: searchParams.schema
-                            }} />
-                            <button class="danger-button" onClick={() => confirmDialog.showModal()}>Delete</button>
+                        <div>
+                            <p class="eyebrow-label">Credential Template</p>
+                            <h1>{credential_manifest.name}</h1>
+                            <p class="details-description">{credential_manifest.description}</p>
+                            <p>
+                                <a class="item-panel-card-link" href={`${location.origin}/apply/${credential_manifest.id}`} target="_blank">
+                                    Application URL <Icon svg={ExternalArrow} />
+                                </a>
+                            </p>
+                            <div class="details-page-hero-content-actions button-row">
+                                <IssueModal content={{
+                                    button: {
+                                        className: "secondary-button",
+                                        label: "Issue"
+                                    },
+                                    manifestId: params.id,
+                                    schemaId: searchParams.schema
+                                }} />
+                                <button class="danger-button" onClick={() => confirmDialog.showModal()}>Delete</button>
+                            </div>
                         </div>
-                        <ConfirmationModal 
+                        <div>
+                            <div class="details-page-hero-card"
+                                style={{
+                                    background: credential_manifest.output_descriptors.styles?.background.color || credential_manifest.issuer.styles?.background.color,
+                                    color: credential_manifest.output_descriptors.styles?.text.color || credential_manifest.issuer.styles?.text.color
+                                }}>
+                                <div class="details-page-hero-card-content">
+                                    <div class="details-page-hero-card-content-hero details-page-hero-card-full-width">
+                                        <img src={credential_manifest.output_descriptors.styles?.hero.url || TBDWordmark} alt={credential_manifest.output_descriptors.styles?.hero.alt || "Generic abstract credential hero image"} />
+                                    </div>
+                                    <div class="details-page-hero-card-content-thumbnail">
+                                        <img src={credential_manifest.output_descriptors.styles?.thumbnail.url || TBDLogoSquare} alt={credential_manifest.output_descriptors.styles?.thumbnail.alt || "Generic abstract credential thumbnail image"} />
+                                    </div>
+                                    <div class="details-page-hero-card-content-info">
+                                        {schemaData && Object.keys(schemaData).map(label => {
+                                            return (
+                                                <div class="info-entry">
+                                                    <div class="info-entry-label">{label}</div>
+                                                    <div class="info-entry-data">Placeholder</div>
+                                                </div>
+                                            )
+                                        })}
+                                        <div class="info-entry">
+                                            <div class="info-entry-label">Issuer</div>
+                                            <div class="info-entry-data">{credential_manifest.issuer.name}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="details-page-hero-card-content">
+                                    <div class="details-page-hero-card-content-header">{credential_manifest.name}</div>
+                                    <div class="details-page-hero-card-content-body">Verifiable Credential</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <ConfirmationModal 
                             ref={confirmDialog}
                             onCancel={() => confirmDialog.close()}
                             onConfirm={() => confirmDelete(credential_manifest)}
@@ -101,7 +104,6 @@ const Details: Component<{ credential }> = (props) => {
                             cancelMessage={"No, cancel"}
                             confirmMessage={"Yes, delete"}
                             />
-                    </div>
                     {editor.dom}
                 </section>
             </div>
