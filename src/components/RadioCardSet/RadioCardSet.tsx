8311dd548da9c775c4833d9dbca2b05f044b47bf
Kirah Sapong <kirah@tbd.email>
2023-07-12 09:22:08 -0700

    Update card styling in card panels

--- src/components/RadioCardSet/RadioCardSet.tsx
+++ src/components/RadioCardSet/RadioCardSet.tsx
@@ -18,29 +18,33 @@ import { Component, For, JSX } from "solid-js";
     activeSelection?
     }> = (props) => {
     return (
-        <fieldset class="radio-card-set field-container">
-            <legend class="field-heading">
-                <label>{props.legend} {props.optional && "(Optional)"}</label>
-            </legend>
-            {props.description && <p class="modal-input-note">{props.description}</p>}
-            <For each={props.options}>
-                {(option) => 
-                    <div class="radio-container">
-                        <input required={!props.optional} checked={option.value === props.activeSelection} oninput={props.handleEvent} type="radio" name={props.name} id={option.value} value={option.value} disabled={option.disabled}/>
-                        <label for={option.value} class="radio-control">
-                            <div class="radio-control-content">
-                                {option.imageSrc && <img class="radio-card-image" src={option.imageSrc} alt="" />}
-                                <div class="radio-control-body">
-                                    <p class="radio-control-heading">{option.label}</p>
-                                    {option.description && <p class="radio-card-description">{option.description}</p>}
-                                    {option.footerLabel && <p class="radio-control-footer-label">{option.footerLabel}</p>}
-                                </div>
+        <div class="field-container">
+            <fieldset class="radio-card-set">
+                <legend class="field-heading">
+                    <label>{props.legend} {props.optional && "(Optional)"}</label>
+                </legend>
+                {props.description && <p class="modal-input-note">{props.description}</p>}
+                <div class="radio-card-options">
+                    <For each={props.options}>
+                        {(option) => 
+                            <div class="radio-container">
+                                <input required={!props.optional} checked={option.value === props.activeSelection} oninput={props.handleEvent} type="radio" name={props.name} id={option.value} value={option.value} disabled={option.disabled}/>
+                                <label for={option.value} class="radio-control">
+                                    <div class="radio-control-content">
+                                        {option.imageSrc && <img class="radio-card-image" src={option.imageSrc} alt="" />}
+                                        <div class="radio-control-body">
+                                            <p class="radio-control-heading">{option.label}</p>
+                                            {option.description && <p class="radio-card-description">{option.description}</p>}
+                                            {option.footerLabel && <p class="radio-control-footer-label">{option.footerLabel}</p>}
+                                        </div>
+                                    </div>
+                                </label>
                             </div>
-                        </label>
-                    </div>
-                }
-            </For>
-        </fieldset>
+                        }
+                    </For>
+                </div>
+            </fieldset>
+        </div>
 
     )
 }
